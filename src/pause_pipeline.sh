4313776faca939144d177e406758eddc155075d5
Zach Maas <zach.maas@gmail.com>
2018-07-10 11:07:55 -0600

    Better documentation for our pipeline script
    Former-commit-id: e5de5dea24ae22a108317e683d089e4802812c97

--- src/pause_pipeline.sh
+++ src/pause_pipeline.sh
@@ -5,19 +5,27 @@
 #SBATCH --mem=128gb
 #SBATCH --mail-user=zama8258@colorado.edu
 
+## Analysis Scripts
 CoreScript=./calc_pausing_indices_core2008.r
 FixwinScript=./calc_pausing_indices_fixwin.sh
+## Visualization Scripts
 VizScript=./figures.r
+## SRR Number
 SRR=SRR2084576
 
-## Echo our scripts
+## Echo the things we're using
 echo $CoreScript
 echo $FixwinScript
+echo $VizScript
 echo $SRR
 
-## Actually run it all
+## Load Required Modules
 module load bedtools
+## Use CORE 2008 Method
 Rscript $CoreScript --srr=$SRR
+## Use CHEN 2015 Fixed Window Method with 2kbp Gene Body
 bash $FixwinScript --pus=100 --pds=300 --gds=2000 --srr=$SRR
+## Use CHEN 2015 Fixed Window Method with 5kbp Gene Body
 bash $FixwinScript --pus=100 --pds=300 --gds=5000 --srr=$SRR
+## Visualize Results
 Rscript $VizScript --srr=$SRR
