e917c17c028d6f91a349d33b5ba4e3d32584bc21
Zach Maas <zach.maas@gmail.com>
2018-07-10 10:59:03 -0600

    Rename Core 2008 Method File for Consistency
    Former-commit-id: 031b50472952cda6e7692584d01726b92b84f0b7

--- src/pause_pipeline.sh
+++ src/pause_pipeline.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=128gb
 #SBATCH --mail-user=zama8258@colorado.edu
 
-CoreScript=./calc_pausing_indices.r
+CoreScript=./calc_pausing_indices_core2008.r
 FixwinScript=./calc_pausing_indices_fixwin.sh
 VizScript=./figures.r
 SRR=SRR2084576
