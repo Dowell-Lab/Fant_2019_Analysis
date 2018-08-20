3bef61be61da771f719f7d65da0f49dd7f10e495
Zach Maas <zach.maas@gmail.com>
2018-08-19 23:50:02 -0600

    Make sure to quote all of our variables to avoid globbing issues.
    Former-commit-id: e0a3f32ad551bb09daf10ee93102f839d5497515

--- src/charli_pi_to_polya.sh
+++ src/charli_pi_to_polya.sh
@@ -37,8 +37,8 @@ usage()
 }
 
 while [ "$1" != "" ]; do
-    PARAM=$(echo $1 | awk -F= '{print $1}')
-		VALUE=$(echo $1 | awk -F= '{print $2}')
+    PARAM=$(echo "$1" | awk -F= '{print $1}')
+		VALUE=$(echo "$1" | awk -F= '{print $2}')
 		case $PARAM in
 				-h | --help)
 						usage
@@ -80,61 +80,61 @@ fi
 DirPrefix=/scratch/Users/zama8258
 TmpDir=charli_pi
 Infile=$DirPrefix/NCBI_RefSeq_UCSC_RefSeq_hg19.bed
-OutFile=$DirPrefix/pause_output/$srr\_pause_ratios_$gds.data
-OutGeneFile=$DirPrefix/$TmpDir/$srr\_$gds\_tss.bed
-OutBodyFile=$DirPrefix/$TmpDir/$srr\_$gds\_body.bed
+OutFile=$DirPrefix/pause_output/"$srr"_pause_ratios_$gds.data
+OutGeneFile=$DirPrefix/$TmpDir/"$srr"_"$gds"_tss.bed
+OutBodyFile=$DirPrefix/$TmpDir/"$srr"_"$gds"_body.bed
 InterestFile=$srr
 #InterestFile=/scratch/Shares/public/nascentdb/processedv2.0/bedgraphs/$srr.tri.BedGraph
-InterestFilePos=$DirPrefix/$TmpDir/$srr\_$gds\_interest_pos.bed
-InterestFileNeg=$DirPrefix/$TmpDir/$srr\_$gds\_interest_neg.bed
+InterestFilePos=$DirPrefix/$TmpDir/"$srr"_"$gds"_interest_pos.bed
+InterestFileNeg=$DirPrefix/$TmpDir/"$srr"_"$gds"_interest_neg.bed
 
 echo Prefiltering Reference Sequence...
 awk -v OFS='\t' -v pus="$pus" -v pds="$pds" '{if ($6 == "+") print $1, $2-pus, $2+pds, $4, $5, $6; else print $1, $3-pus, $3+pds, $4, $5, $6}' $Infile \
-		| sort -u -k1,1 -k2,2n | awk -v OFS='\t' '{if ($2 < $3) print $1, $2, $3, $4}' > $OutBodyFile &
+		| sort -u -k1,1 -k2,2n | awk -v OFS='\t' '{if ($2 < $3) print $1, $2, $3, $4}' > "$OutBodyFile" &
 awk -v OFS='\t' -v gus="$gus" -v gds="$gds" '{if ($6 == "+") print $1, $2+gus, $3, $4, $5, $6; else print $1, $3-gus, $3, $4, $5, $6}' $Infile \
-		| sort -u -k1,1 -k2,2n | awk -v OFS='\t' '{if ($2 < $3) print $1, $2, $3, $4}' > $OutGeneFile &
+		| sort -u -k1,1 -k2,2n | awk -v OFS='\t' '{if ($2 < $3) print $1, $2, $3, $4}' > "$OutGeneFile" &
 wait
 
 echo Splitting data file into pos and neg...
-awk -v OFS='\t' '{if ($4 > 0) print $1, $2, $3, $4}' $InterestFile > $InterestFilePos &
-awk -v OFS='\t' '{if ($4 < 0) print $1, $2, $3, $4}' $InterestFile > $InterestFileNeg &
+awk -v OFS='\t' '{if ($4 > 0) print $1, $2, $3, $4}' "$InterestFile" > "$InterestFilePos" &
+awk -v OFS='\t' '{if ($4 < 0) print $1, $2, $3, $4}' "$InterestFile" > "$InterestFileNeg" &
 wait
 
 # Output Filenames
-GeneOutPos=$DirPrefix/$TmpDir/$srr\_$gds\_out_gene_pos.bed
-GeneOutNeg=$DirPrefix/$TmpDir/$srr\_$gds\_out_gene_neg.bed
-BodyOutPos=$DirPrefix/$TmpDir/$srr\_$gds\_out_body_pos.bed
-BodyOutNeg=$DirPrefix/$TmpDir/$srr\_$gds\_out_body_neg.bed
+GeneOutPos=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_gene_pos.bed
+GeneOutNeg=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_gene_neg.bed
+BodyOutPos=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_body_pos.bed
+BodyOutNeg=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_body_neg.bed
 
 echo Finding Region Sums...
-bedtools map -a $OutGeneFile -b $InterestFilePos -c 4 -o sum \
-		| awk '($5 != "." && $5 != 0) ' > $GeneOutPos &
-bedtools map -a $OutBodyFile -b $InterestFilePos -c 4 -o sum \
-		| awk '($5 != "." && $5 != 0) ' > $BodyOutPos &
-bedtools map -a $OutGeneFile -b $InterestFileNeg -c 4 -o sum \
-		| awk '($5 != "." && $5 != 0) ' > $GeneOutNeg &
-bedtools map -a $OutBodyFile -b $InterestFileNeg -c 4 -o sum \
-		| awk '($5 != "." && $5 != 0) ' > $BodyOutNeg &
+bedtools map -a "$OutGeneFile" -b "$InterestFilePos" -c 4 -o sum \
+		| awk '($5 != "." && $5 != 0) ' > "$GeneOutPos" &
+bedtools map -a "$OutBodyFile" -b "$InterestFilePos" -c 4 -o sum \
+		| awk '($5 != "." && $5 != 0) ' > "$BodyOutPos" &
+bedtools map -a "$OutGeneFile" -b "$InterestFileNeg" -c 4 -o sum \
+		| awk '($5 != "." && $5 != 0) ' > "$GeneOutNeg" &
+bedtools map -a "$OutBodyFile" -b "$InterestFileNeg" -c 4 -o sum \
+		| awk '($5 != "." && $5 != 0) ' > "$BodyOutNeg" &
 wait
 
-CoverageOutPos=$DirPrefix/$TmpDir/$srr\_$gds\_out_coverage_pos.bed
-CoverageOutNeg=$DirPrefix/$TmpDir/$srr\_$gds\_out_coverage_neg.bed
+CoverageOutPos=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_coverage_pos.bed
+CoverageOutNeg=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_coverage_neg.bed
 
 # FIXME - Finish Coverage Statistics
 echo Calculating Coverage Statistics
-awk 'NR==FNR{a[NR]=$5;next}{print $5+a[FNR]}' $GeneOutPos $BodyOutPos > $CoverageOutPos	&
-awk 'NR==FNR{a[NR]=$5;next}{print $5+a[FNR]}' $GeneOutNeg $BodyOutNeg > $CoverageOutNeg	&
+awk 'NR==FNR{a[NR]=$5;next}{print $5+a[FNR]}' "$GeneOutPos" "$BodyOutPos" > "$CoverageOutPos"	&
+awk 'NR==FNR{a[NR]=$5;next}{print $5+a[FNR]}' "$GeneOutNeg" "$BodyOutNeg" > "$CoverageOutNeg"	&
 wait
 
-FinalPos=$DirPrefix/$TmpDir/$srr\_$gds\_out_final_pos.bed
-FinalNeg=$DirPrefix/$TmpDir/$srr\_$gds\_out_final_neg.bed
+FinalPos=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_final_pos.bed
+FinalNeg=$DirPrefix/$TmpDir/"$srr"_"$gds"_out_final_neg.bed
 
 # Add	Coverage to	Bodies
-paste $BodyOutPos $CoverageOutPos > $FinalPos	&
-paste $BodyOutNeg $CoverageOutNeg > $FinalNeg &
+paste "$BodyOutPos" "$CoverageOutPos" > "$FinalPos"	&
+paste "$BodyOutNeg" "$CoverageOutNeg" > "$FinalNeg" &
 wait
 
 echo Calculating Pausing Index
-awk -F '\t' 'FNR==NR{a[$4]=$5; next} ($4 in a) {print $4,"+",$5/a[$4], $6}' $GeneOutPos $FinalPos > $OutFile
-awk -F '\t' 'FNR==NR{a[$4]=$5; next} ($4 in a) {print $4,"-",$5/a[$4], $6}' $GeneOutNeg $FinalNeg >> $OutFile
+awk -F '\t' 'FNR==NR{a[$4]=$5; next} ($4 in a) {print $4,"+",$5/a[$4], $6}' "$GeneOutPos" "$FinalPos" > "$OutFile"
+awk -F '\t' 'FNR==NR{a[$4]=$5; next} ($4 in a) {print $4,"-",$5/a[$4], $6}' "$GeneOutNeg" "$FinalNeg" >> "$OutFile"
 echo "Done $srr $gds"
