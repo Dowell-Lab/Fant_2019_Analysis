a6b8b501066c7e0ecb083760a4f7705222bd8850
Zach Maas <zach.maas@gmail.com>
2019-03-17 13:50:15 -0600

    Update the code used for metagene analysis to work all at once.
    - Reduce the number of cores used by the gen_featurecounts script.
    - Generate all 3 counts tables needed at once.
    - Use SAF files instead of the GTF files, simplifying the code
    - Generate figures for all 3 conditions in one step
    
    
    Former-commit-id: 281447f45bbf27be3c7e22d94068542ab0daa9aa

--- src/run_deseq.r
+++ src/run_deseq.r
@@ -1,5 +1,5 @@
-## Script to run DESeq2 Maintainter: Zachary Maas <zama8258@colorado.edu> Licensed
-## under GPLv3
+## Script to run DESeq2
+## Maintainer: Zachary Maas <zama8258@colorado.edu>
 
 #######################
 ## Preliminary Setup ##
@@ -8,28 +8,56 @@
 library("tidyverse")
 library("DESeq2")
 
+## Set up a function for plotting
+makefig <- function(deseqdata, fileprefix) {
+    res <- DESeq2::results(deseqdata)
+    table(res$padj < 0.05)
+    ## Order by adjusted p-value
+    res <- res[order(res$padj), ]
+    ## Merge with normalized count data
+    resdata <- merge(as.data.frame(res), as.data.frame(counts(deseqdata, normalized = TRUE)),
+                     by = "row.names", sort = FALSE)
+    names(resdata)[1] <- "Gene"
+    head(resdata)
+    ## Write results
+    write.csv(resdata, file = paste0(fileprefix, "-diffexpr-results.csv"))
+
+    ptsize <- 60
+
+    ## png(paste0(fileprefix, "-diffexpr-hist.png"), 1500, 1000, pointsize = ptsize)
+    ## hist(res$pvalue, breaks = 50, col = "grey")
+    ## dev.off()
+
+    ## png('MA_plot.png') plotMA(res) dev.off()
+
+    png(paste0(fileprefix, "-diffexpr-maplot.png"), 3000, 2000, pointsize = ptsize)
+    DESeq2::plotMA(res, main = "MA Plot")
+    dev.off()
+
+    ## Generate a tsv
+    write.table(na.omit(as.data.frame(res[2])),
+                file = paste0(fileprefix, "-genes_for_gsea.txt"), row.names = TRUE,
+                quote = FALSE)
+    write.table(res, file = paste0(fileprefix, "-DESeq.res.txt"), append = FALSE, sep = "\t")
+}
 
 ## To filter out only the genes we've used and convert to common ID
 idConvert <- read_delim("refseq_to_common_id.txt",
                         col_names = c("rowname", "common"), delim = " ")
-## ids <- read_delim("C413_1_S3_R1_001.trim.bedGraph_pause_ratios_PROD.data",
-##                   col_names = c("rowname", "strand", "c_1_PROD", "coverage_c1_PROD"), delim = " ") %>%
-##     subset(select = rowname)
-
-prefix="initiation"
+## Experimental condition
+condition <- factor(c(rep("treated", 2), rep("untreated", 2)), levels=c("untreated", "treated"))
 
 #################################################
 ## Initial Processing for Correct Size Factors ##
 #################################################
 
+## Here, we use the genebody only and save appropriate plots
+
 ## We have to fix the counts table by removing the first row, hence "counts_fix.txt"
-correct_seqdata <- read.delim("counts_fullgene.txt_without_header", stringsAsFactors = FALSE, header = TRUE,
+correct_seqdata <- read.delim("counts_genebody.txt_without_header", stringsAsFactors = FALSE, header = TRUE,
                               row.names = 1)
 ## Then, we filter to only include the resolved isoforms
 correct_df <- as.tibble(rownames_to_column(correct_seqdata))
-## correct_df <- inner_join(ids, correct_df, by="rowname")
-## correct_df <- inner_join(correct_df, idConvert, by = "rowname") %>% subset(select = -c(rowname)) %>%
-##     distinct(common, .keep_all = TRUE)
 correct_dt <- data.frame(correct_df)
 rownames(correct_dt) <- correct_dt$common
 correct_dt$common <- NULL
@@ -39,11 +67,13 @@ correct_countdata <- correct_dt[, 6:ncol(correct_dt)]
 correct_countdata <- as.matrix(correct_countdata)
 correct_countdata <- correct_countdata %>% subset(select = -c(Length))
 
-condition <- factor(c(rep("treated", 2), rep("untreated", 2)), levels=c("untreated", "treated"))
 correct_coldata <- data.frame(row.names = colnames(correct_countdata), condition)
 
 cds <- DESeqDataSetFromMatrix(countData = correct_countdata, colData = correct_coldata, design = ~condition)
 cds <- DESeq(cds)
+
+## Make a figure for this data
+makefig(cds, "genebody")
 
 ## Extract the size factors
 sizes <- c(sizeFactors(cds))
@@ -53,7 +83,7 @@ sizes <- c(sizeFactors(cds))
 ###########################################################
 
 ## We have to fix the counts table by removing the first row, hence "counts_fix.txt"
-full_seqdata <- read.delim("counts_initiation_fix.txt", stringsAsFactors = FALSE, header = TRUE,
+full_seqdata <- read.delim("counts_fullgene.txt_without_header", stringsAsFactors = FALSE, header = TRUE,
                            row.names = 1)
 ## Then, we filter to only include the resolved isoforms
 full_df <- as.tibble(rownames_to_column(full_seqdata))
@@ -69,73 +99,39 @@ full_countdata <- full_dt[, 6:ncol(full_dt)]
 full_countdata <- as.matrix(full_countdata)
 full_countdata <- full_countdata %>% subset(select = -c(Length))
 
-condition <- factor(c(rep("treated", 2), rep("untreated", 2)), levels=c("untreated", "treated"))
 full_coldata <- data.frame(row.names = colnames(full_countdata), condition)
 
 dds <- DESeqDataSetFromMatrix(countData = full_countdata, colData = full_coldata, design = ~condition)
 sizeFactors(dds) <- sizes
 dds <- DESeq(dds)
 
+makefig(dds, "fullgene")
+
 ######################################################
-## With Proper Estimates, we can generate a MA Plot ##
+## Finally, use the initiation region only          ##
 ######################################################
 
-res <- results(dds)
-table(res$padj < 0.05)
-## Order by adjusted p-value
-res <- res[order(res$padj), ]
-## Merge with normalized count data
-resdata <- merge(as.data.frame(res), as.data.frame(counts(dds, normalized = TRUE)), 
-    by = "row.names", sort = FALSE)
-names(resdata)[1] <- "Gene"
-head(resdata)
-## Write results
-write.csv(resdata, file = "diffexpr-results.csv")
+## We have to fix the counts table by removing the first row, hence "counts_fix.txt"
+initiation_seqdata <- read.delim("counts_initiation.txt_without_header", stringsAsFactors = FALSE, header = TRUE,
+                                 row.names = 1)
+## Then, we filter to only include the resolved isoforms
+initiation_df <- as.tibble(rownames_to_column(initiation_seqdata))
+## initiation_df <- inner_join(ids, initiation_df, by="rowname")
+## initiation_df <- inner_join(initiation_df, idConvert, by = "rowname") %>% subset(select = -c(rowname)) %>%
+## distinct(common, .keep_all = TRUE)
+initiation_dt <- data.frame(initiation_df)
+rownames(initiation_dt) <- initiation_dt$common
+initiation_dt$common <- NULL
 
-ptsize <- 25
+## Now, actually perform DE-Seq
+initiation_countdata <- initiation_dt[, 6:ncol(initiation_dt)]
+initiation_countdata <- as.matrix(initiation_countdata)
+initiation_countdata <- initiation_countdata %>% subset(select = -c(Length))
 
-png(paste0(prefix, "-diffexpr-hist.png"), 1500, 1000, pointsize = ptsize)
-hist(res$pvalue, breaks = 50, col = "grey")
-dev.off()
+initiation_coldata <- data.frame(row.names = colnames(initiation_countdata), condition)
 
-## png('MA_plot.png') plotMA(res) dev.off()
+eds <- DESeqDataSetFromMatrix(countData = initiation_countdata, colData = initiation_coldata, design = ~condition)
+sizeFactors(eds) <- sizes
+eds <- DESeq(eds)
 
-png(paste0(prefix, "-diffexpr-maplot.png"), 1500, 1000, pointsize = ptsize)
-plotMA(res, main = "MA Plot")
-dev.off()
-
-## Volcano plot with 'significant' genes labeled
-## volcanoplot <- function(res, lfcthresh = 2, sigthresh = 0.05, main = "Volcano Plot",
-##     legendpos = "bottomright", labelsig = TRUE, textcx = 1, ...) {
-##     with(res, plot(log2FoldChange, -log10(pvalue), pch = 20, main = main, ...))
-##     with(subset(res, padj < sigthresh), points(log2FoldChange, -log10(pvalue), pch = 20,
-##         col = "red", ...))
-##     with(subset(res, abs(log2FoldChange) > lfcthresh), points(log2FoldChange, -log10(pvalue),
-##         pch = 20, col = "orange", ...))
-##     with(subset(res, padj < sigthresh & abs(log2FoldChange) > lfcthresh), points(log2FoldChange,
-##         -log10(pvalue), pch = 20, col = "green", ...))
-##     if (labelsig) {
-##         require(calibrate)
-##         with(subset(res, padj < sigthresh & abs(log2FoldChange) > lfcthresh), textxy(log2FoldChange,
-##             -log10(pvalue), labs = Gene, cex = textcx, ...))
-##     }
-##     legend(legendpos, xjust = 1, yjust = 1, legend = c(paste("FDR<", sigthresh, sep = ""),
-##         paste("|LogFC|>", lfcthresh, sep = ""), "both"), pch = 20, col = c("red",
-##         "orange", "green"))
-## }
-## png("diffexpr-volcanoplot.png", 1200, 1000, pointsize = 20)
-## volcanoplot(resdata, lfcthresh = 1, sigthresh = 0.05, textcx = 0.8, xlim = c(-2.3,
-##     2))
-## dev.off()
-
-cat(rownames(subset(subset(res, pvalue < 0.01), log2FoldChange < 0)), sep = "\n")
-cat(rownames(subset(subset(res, pvalue < 0.01), log2FoldChange > 0)), sep = "\n")
-
-length(rownames(subset(subset(res, pvalue < 0.01), log2FoldChange < 0)))
-length(rownames(subset(subset(res, pvalue < 0.01), log2FoldChange > 0)))
-## Somme good genes: TOP: NM_001281749
-
-## Generate a tsv
-write.table(na.omit(as.data.frame(res[2])), file = "genes_for_gsea.txt", row.names = TRUE, 
-            quote = FALSE)
-write.table(res, file = "DESeq.res.txt", append = FALSE, sep = "\t")
+makefig(eds, "initiation")
